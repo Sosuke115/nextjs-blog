e4364db142dddf46902fa5a0d151a7d162b09119
sosuke115 <sosuken115@gmail.com>
2022-12-29 23:36:00 +0900

    トップページにはプロフィールのみのせる

--- components/profile.js
+++ components/profile.js
@@ -40,8 +40,6 @@ export default function Profile(props) {
             </a>
             , 2020
             <br />
-            {/* <a href="https://sosuke115.github.io/">Here</a> is the previous
-            portfolio. */}
           </p>
         </div>
       </div>
@@ -62,9 +60,11 @@ export default function Profile(props) {
 
         <div className={styles.profileTextWrapper}>
           <div className={`${utilStyles.headingXl} ${styles.profileName}`}>
-            {props.name}
+            プロフィール
           </div>
           <p className={`${utilStyles.headingMd} ${styles.profileText}`}>
+            西川 荘介（にしかわ そうすけ）
+            <br />
             データサイエンティスト,{" "}
             <Link href="https://about.yahoo.co.jp/">ヤフー株式会社</Link>
             <br />
@@ -74,12 +74,10 @@ export default function Profile(props) {
             , 2022
             <br />
             <a href="https://www.ee.t.u-tokyo.ac.jp/j/">
-              東京大学 工学部 電子情報工学科卒
+              東京大学 工学部 電子情報工学科卒業
             </a>
             , 2020
             <br />
-            {/* 学生時代のポートフォリオは
-            <a href="https://sosuke115.github.io/">コチラ</a> */}
           </p>
         </div>
       </div>
--- pages/ja/index.js
+++ pages/ja/index.js
@@ -1,45 +1,21 @@
 import Head from "next/head";
 import utilStyles from "../../styles/utils.module.css";
-import { getSortedPostsData } from "../../lib/posts";
+import { getStaticPropsLib } from "../../lib/posts";
 import Profile from "../../components/profile";
 import Layout, { siteTitle } from "../../components/layout";
 
 export async function getStaticProps() {
-  const allPostsData = await getSortedPostsData();
-  const idToData = { contentHtml: {}, title: {} };
-  allPostsData.forEach((data) => {
-    idToData["contentHtml"][data["id"]] = data["contentHtml"];
-    idToData["title"][data["id"]] = data["title"];
-  });
-  return {
-    props: {
-      idToData,
-    },
-  };
+  return getStaticPropsLib();
 }
+
 export default function Home({ idToData }) {
-  const idList = ["経歴", "研究業績", "開発物", "賞"];
+  const idList = ["アバウト"];
   return (
     <Layout>
       <Head>
         <title>西川荘介のポートフォリオ</title>
       </Head>
-
-      <Profile name="西川 荘介" />
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <ul className={utilStyles.list}>
-          {idList.map((id) => (
-            <li key={id} className={utilStyles.listItem}>
-              <h1 className={utilStyles.headBorder}>{idToData.title[id]}</h1>
-              <div
-                dangerouslySetInnerHTML={{ __html: idToData.contentHtml[id] }}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-      
+      <Profile name="西川 荘介" />   
     </Layout>
   );
 }
